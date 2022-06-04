53322ac5d88c72831728608973ceb3eb8c7386df
thuham2k4 <dinhthithu20042k1@gmail.com>
2022-06-04 21:14:11 +0700

    let dog eat cat

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var dog = require ('./dog');
+var cat = require('./cat');
 var otherdog = new dog('Tom đẹp trai');
 otherdog.sayHi();
+var tom = new cat();
+otherdog.eat(tom);
+console.log(otherdog);
