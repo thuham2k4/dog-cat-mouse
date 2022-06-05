9de760f32041859d83886ccc544793993fdca8b0
thuham2k4 <dinhthithu20042k1@gmail.com>
2022-06-05 17:33:01 +0700

    improve cat eat method

--- cat.js
+++ cat.js
@@ -1,7 +1,14 @@
+var mouse = require('./mouse');
 function cat() { 
     this.stomach =[];
  }
- cat.prototype.eat = function(mouse){
-     this.stomach.push(mouse);
+ cat.prototype.eat = function(animal){
+     if (animal instanceof mouse ){
+        this.stomach.push(animal);
+     }
+     else {
+         throw new Error ('cat can only eat mouse !')
+     }
+     
  }
  module.exports = cat
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var dog = require ('./dog');
 var cat = require('./cat');
+var mouse = require('./mouse');
+
 var otherdog = new dog('Tom đẹp trai');
 otherdog.sayHi();
 var tom = new cat();
 otherdog.eat(tom);
 console.log(otherdog);
+var cat = new cat();
+var mouse = new mouse('minh hieu thúi');
+cat.eat(mouse);
+console.log(cat);
+cat.eat(otherdog);
